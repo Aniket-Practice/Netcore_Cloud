2b8fd88ac8aefaa360e2116f9421f16a837ddec8
Aniket Practice <waghmareaniket150@gmail.com>
2024-03-04 20:50:22 +0530

    My profile is UptoDate

---
+++ NetcoreQA.java
@@ -0,0 +1,8 @@
+package Netcore;
+
+public class NetcoreQA {
+    public static void main(String[] args) {
+        System.out.println("I am Aniket Waghmare");
+        System.out.println("Designation: QA Manager");
+    }
+}
